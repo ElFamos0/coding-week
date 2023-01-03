b2a7a6c47675b1194518bb081ce88b1d7efcf2e1
Malo DAMIEN <malo.damien@telecomnancy.eu>
2023-01-03 11:57:02 +0100

    [+] modèle + poiro

---
+++ src/main/java/com/amplet/app/Model.java
@@ -0,0 +1,15 @@
+package com.amplet.app;
+
+import java.util.ArrayList;
+
+public class Model {
+    ArrayList<Pile> allpiles;
+    ArrayList<Carte> allcartes;
+
+    public Model() {}
+
+    public void mangemoilepoiro() {
+        System.out.println("mangemoilepoiro");
+    }
+
+}
