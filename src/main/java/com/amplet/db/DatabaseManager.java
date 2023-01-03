369f17a24bee3cc7ec75bed50559fbb59dca1cd0
Tom BÉNÉ <tom.bene@telecomnancy.eu>
2023-01-03 14:06:25 +0100

    [+] class to interface with db

---
+++ src/main/java/com/amplet/db/DatabaseManager.java
@@ -0,0 +1,22 @@
+package com.amplet.db;
+
+import java.sql.SQLException;
+import com.j256.ormlite.jdbc.JdbcPooledConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+public class DatabaseManager {
+
+    private JdbcPooledConnectionSource connectionSource;
+
+    public DatabaseManager() throws SQLException {
+        String tempDir = System.getProperty("java.io.tmpdir");
+        this.connectionSource =
+                new JdbcPooledConnectionSource("jdbc:sqlite:" + tempDir + "/amplet.db");
+        TableUtils.createTableIfNotExists(this.connectionSource, Carte.class);
+    }
+
+    public void closeConnection() {
+        this.connectionSource.closeQuietly();
+    }
+
+}
