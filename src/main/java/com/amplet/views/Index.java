23e73016d8b7534ee79f785513dc83704abe629e
Malo DAMIEN <malo.damien@telecomnancy.eu>
2023-01-03 15:20:42 +0100

    [+] observer et initialisable

--- src/main/java/com/amplet/views/Index.java
+++ src/main/java/com/amplet/views/Index.java
@@ -1,4 +1,13 @@
 package com.amplet.views;
 
-public class Index {
+import com.amplet.app.Observer;
+import javafx.fxml.Initializable;
+
+public class Index implements Observer, Initializable {
+
+    public void update() {
+        System.out.println("Index updated");
+    }
+
+    public void initialize(java.net.URL location, java.util.ResourceBundle resources) {}
 }
--- src/main/java/com/amplet/views/Menu.java
+++ src/main/java/com/amplet/views/Menu.java
@@ -2,9 +2,11 @@ package com.amplet.views;
 
 import java.io.IOException;
 import com.amplet.app.App;
+import com.amplet.app.Observer;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
-public class Menu {
+public class Menu implements Observer, Initializable {
 
     @FXML
     private void retourAccueil() throws IOException {
@@ -16,4 +18,10 @@ public class Menu {
     private void test() {
         System.out.println("Retour à l'accueil");
     }
+
+    public void update() {
+        System.out.println("Menu updated");
+    }
+
+    public void initialize(java.net.URL location, java.util.ResourceBundle resources) {}
 }
