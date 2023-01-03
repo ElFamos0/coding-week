05aac197ca46ffced9f3ffbabbe6263b1e91d364
Malo DAMIEN <malo.damien@telecomnancy.eu>
2023-01-03 18:00:05 +0100

    [+] flippendo

--- src/main/java/com/amplet/views/ApprIg.java
+++ src/main/java/com/amplet/views/ApprIg.java
@@ -1,6 +1,86 @@
 package com.amplet.views;
 
 
-public class ApprIg {
+import com.amplet.app.Observer;
+import javafx.animation.RotateTransition;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.transform.Rotate;
+import javafx.util.Duration;
+
+
+
+public class ApprIg implements Observer {
+
+    @FXML
+    private VBox carte;
+
+    private VBox carteFront;
+    private VBox carteBack;
+    private boolean isFront = true;
+    private boolean isFlipping = false;
+
+
+    @FXML
+    public void initialize() {
+        carteFront = new VBox();
+        carteFront.getChildren().addAll(carte.getChildren());
+        carte.getChildren().clear();
+        carte.getChildren().addAll(carteFront);
+    }
+
+    @FXML
+    private void flipCard() {
+        if (isFlipping) {
+            return;
+        }
+        isFlipping = true;
+        carteBack = new VBox();
+        // Vertically center a label
+        Label backLabel = new Label("Back");
+        backLabel.setTranslateY(-backLabel.getHeight() / 2);
+        carteBack.getChildren().addAll(backLabel);
+        if (isFront) {
+            RotateTransition rotateTransition = new RotateTransition(Duration.seconds(0.5), carte);
+            rotateTransition.setByAngle(90);
+            rotateTransition.setAxis(Rotate.X_AXIS);
+            rotateTransition.play();
+            rotateTransition.setOnFinished(event -> {
+                // Put upside down
+                RotateTransition flip = new RotateTransition(Duration.seconds(0.5), carte);
+                flip.setByAngle(-90);
+                flip.setAxis(Rotate.X_AXIS);
+                flip.play();
+                carte.getChildren().clear();
+                carte.getChildren().addAll(carteBack);
+                flip.setOnFinished(event1 -> {
+                    isFlipping = false;
+                });
+            });
+        } else {
+            RotateTransition rotateTransition = new RotateTransition(Duration.seconds(0.5), carte);
+            rotateTransition.setByAngle(90);
+            rotateTransition.setAxis(Rotate.X_AXIS);
+            rotateTransition.play();
+            rotateTransition.setOnFinished(event -> {
+                // Put upside down
+                RotateTransition flip = new RotateTransition(Duration.seconds(0.5), carte);
+                flip.setByAngle(-90);
+                flip.setAxis(Rotate.X_AXIS);
+                flip.play();
+                carte.getChildren().clear();
+                carte.getChildren().addAll(carteFront);
+                flip.setOnFinished(event1 -> {
+                    isFlipping = false;
+                });
+            });
+        }
+        isFront = !isFront;
+    }
+
+    public void update() {
+        // TODO
+    }
 
 }
