26591628fead45df6a5438da9a2401711c79bb94
Tom BÉNÉ <tom.bene@telecomnancy.eu>
2023-01-03 12:32:22 +0100

    [+] requires in module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,12 @@ module com.amplet {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires ormlite.jdbc;
+    requires java.sql;
+
     opens com.amplet.app to javafx.fxml;
     opens com.amplet.views to javafx.fxml;
+
     exports com.amplet.views;
     exports com.amplet.app;
 }
