0a8b4ccaad16be3a9bff2709c5d59accbc945850
BRUNET Jules <jules.brunet@telecomnancy.eu>
2023-01-04 08:35:58 +0100

    [+] apprParam.fxml

---
+++ src/main/java/com/amplet/views/ApprParam.java
@@ -0,0 +1,19 @@
+package com.amplet.views;
+
+import com.amplet.app.Observer;
+import javafx.fxml.FXML;
+
+public class ApprParam implements Observer {
+
+    @FXML
+    public void initialize() {
+
+    }
+
+
+
+    public void update() {
+
+
+    }
+}
