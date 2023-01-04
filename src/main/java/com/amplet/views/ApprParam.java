15de490768cb86e8ed27140f25e12c00e78d9191
BRUNET Jules <jules.brunet@telecomnancy.eu>
2023-01-04 09:49:00 +0100

    [+] Functionning index buttons

--- src/main/java/com/amplet/views/ApprParam.java
+++ src/main/java/com/amplet/views/ApprParam.java
@@ -67,7 +67,6 @@ public class ApprParam implements Observer {
     }
 
 
-
     public void update() {
 
 
--- src/main/java/com/amplet/views/Index.java
+++ src/main/java/com/amplet/views/Index.java
@@ -1,13 +1,43 @@
 package com.amplet.views;
 
+import java.io.IOException;
+import com.amplet.app.App;
 import com.amplet.app.Observer;
-import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
 
-public class Index implements Observer, Initializable {
+public class Index implements Observer {
+
+    @FXML
+    public void switchToCreatePile() throws IOException {
+        System.out.println("Création de pile");
+        App.setRoot("editionPile");
+    }
+
+    @FXML
+    public void switchToModifyPile() throws IOException {
+        System.out.println("Edition de pile");
+        App.setRoot("editionPile");
+    }
+
+    @FXML
+    public void switchToApprParam() throws IOException {
+        System.out.println("ApprParam");
+        App.setRoot("apprParam");
+
+
+    }
+
+    @FXML
+    public void switchToStatistiques() throws IOException {
+        System.out.println("Stas");
+        App.setRoot("statistiques");
+    }
+
+
 
     public void update() {
         System.out.println("Index updated");
     }
 
-    public void initialize(java.net.URL location, java.util.ResourceBundle resources) {}
+    public void initialize() {}
 }
