6291d579bedc964cf7f203d855fe024fb8baed93
Tom BÉNÉ <tom.bene@telecomnancy.eu>
2023-01-04 17:40:10 +0100

    [+] select menu

--- src/main/java/com/amplet/views/Statistiques.java
+++ src/main/java/com/amplet/views/Statistiques.java
@@ -1,9 +1,13 @@
 package com.amplet.views;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.amplet.app.Model;
+import com.amplet.app.Pile;
 import com.amplet.app.ViewController;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.ChoiceBox;
 
 public class Statistiques extends ViewController {
     public Statistiques(Model model) {
@@ -16,54 +20,25 @@ public class Statistiques extends ViewController {
         return this.getClass().getName();
     }
 
+    private List<Pile> piles;
+    private List<String> pilesNames = new ArrayList<String>();
+    private Pile selectedPile;
+
     @FXML
     public void initialize() {
-
+        this.piles = model.getAllPiles();
+        this.selectedPile = this.piles.get(0);
+        this.piles.forEach(pile -> this.pilesNames.add(pile.getNom()));
+        this.choiceBoxPile.getItems().clear();
+        this.choiceBoxPile.getItems().addAll(this.pilesNames);
+        this.choiceBoxPile.setValue(selectedPile.getNom());
     }
 
     @FXML
-    private Label labelQuestion;
-
-    @FXML
-    private Label labelTitre;
-
-    @FXML
-    private Label labelReponse;
-
-    @FXML
-    public void validerTitre() {
-
-    }
-
-    @FXML
-    public void validerQuestion() {
-
-    }
-
-    @FXML
-    public void validerReponse() {
-
-    }
-
-    @FXML
-    public void retour() {
-
-    }
-
-    @FXML
-    public void dupliquerCarte() {
-
-    }
-
-    @FXML
-    public void supprimerCarte() {
-
-    }
+    private ChoiceBox<String> choiceBoxPile;
 
     public void update() {
 
     }
 
-
 }
-
