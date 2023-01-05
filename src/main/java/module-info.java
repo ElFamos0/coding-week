ae1739ab7141f726a0fda4f99e5ee289adde8ea5
Tom BÉNÉ <tom.bene@telecomnancy.eu>
2023-01-05 09:13:47 +0100

    [+] gson in module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,10 +4,12 @@ module com.amplet {
 
     requires ormlite.jdbc;
     requires java.sql;
+    requires com.google.gson;
 
     opens com.amplet.app to javafx.fxml;
     opens com.amplet.views to javafx.fxml;
     opens com.amplet.db to ormlite.jdbc;
+    opens com.amplet.io to com.google.gson;
 
     exports com.amplet.db;
     exports com.amplet.views;
